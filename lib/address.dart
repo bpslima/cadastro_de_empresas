408edb338a481d83fee86f5d71cfb83f11b8cda9
bpslima <beatriz.pslima@gmail.com>
2022-09-07 13:06:38 -0300

    adding class Address

---
+++ lib/address.dart
@@ -0,0 +1,32 @@
+
+
+class Address{
+  //Attributes:
+  String street;
+  String number;
+  String? complement;
+  String district;
+  String state;
+  String zipCode;
+
+  //Constructor:
+  Address(
+    this.street,
+    this.number, 
+    this.complement,
+    this.district,
+    this.state,
+    this.zipCode,
+  );
+
+
+
+
+  
+}
+
+
+
+
+
+//Logradouro, Número, Complemento, Bairro, Estado e CEP
